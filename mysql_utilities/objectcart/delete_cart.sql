263bd135d936c1d59e81cd261c1acdf521999d66
James Tan <james.tan@nih.gov>
2015-04-14 13:29:02 -0400

    JR208

--- mysql_utilities/objectcart/delete_cart.sql
+++ mysql_utilities/objectcart/delete_cart.sql
@@ -1,4 +1,4 @@
 DELETE FROM OBJECT_CART.CART WHERE USER_ID like 'PUBLIC%' and name = 'cdeCart'
 #select * FROM OBJECT_CART.CART WHERE USER_ID like 'PUBLIC%' and name = 'cdeCart'
-and last_write_date > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 180 DAY))
+and last_write_date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 DAY);
 
--- mysql_utilities/objectcart/delete_cartobject.sql
+++ mysql_utilities/objectcart/delete_cartobject.sql
@@ -4,7 +4,6 @@ select a.id from (
 #select c.id as cart_id, co.cart_id as co_cart_id, co.id as co_obj_id from cart_object co,cart c where c.ID=co.CART_ID 
 select c.id from cart_object co, cart c where c.ID=co.CART_ID 
 and USER_ID like 'PUBLIC%' and name = 'cdeCart'
-and c.last_write_date > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 180 DAY))
+and c.last_write_date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 DAY)
 ) a
 )
-
