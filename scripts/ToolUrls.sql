c72b912bc2457b4846aa60874dc8c639b09bff5f
purnimac <purnimac@yahoo.com>
2016-04-29 11:24:23 -0400

    Create ToolUrls.sql
    Environment tool URLs to be updated in 11g

---
+++ scripts/ToolUrls.sql
@@ -0,0 +1,3 @@
+update sbr.tool_options_ext set value = 'https://formbuilder-dev-sg.nci.nih.gov' where tool_name = 'FormBuilder' and property = 'URL';
+
+update sbr.tool_options_ext set value = 'https://cadsrpasswordchange-dev-sg.nci.nih.gov' where tool_name = 'CDEBrowser' and property = 'PCS_URL';
