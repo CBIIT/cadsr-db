098621497caccd5ac3c0e4b040709760a31a1305
purnimac <purnimac@yahoo.com>
2016-04-29 12:08:38 -0400

    Update ToolUrls.sql
    Updated it to QA properties.

--- scripts/ToolUrls.sql
+++ scripts/ToolUrls.sql
@@ -1,3 +1,3 @@
-update sbr.tool_options_ext set value = 'https://formbuilder-dev-sg.nci.nih.gov' where tool_name = 'FormBuilder' and property = 'URL';
+update sbr.tool_options_ext set value = 'https://formbuilder-qa-sg.nci.nih.gov' where tool_name = 'FormBuilder' and property = 'URL';
 
-update sbr.tool_options_ext set value = 'https://cadsrpasswordchange-dev-sg.nci.nih.gov' where tool_name = 'CDEBrowser' and property = 'PCS_URL';
+update sbr.tool_options_ext set value = 'https://cadsrpasswordchange-qa-sg.nci.nih.gov' where tool_name = 'CDEBrowser' and property = 'PCS_URL';
