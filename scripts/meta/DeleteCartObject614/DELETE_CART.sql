494417fbcef6542bcb0ea1472cd75f686171bc54
trushi2 <irina.trush@nih.gov>
2016-10-05 12:57:32 -0400

    Delete DELETE_CART.sql

--- scripts/meta/DeleteCartObject614/DELETE_CART.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM OBJECT_CART.CART WHERE USER_ID like 'PUBLIC%' and name = 'cdeCart'
-#select * FROM OBJECT_CART.CART WHERE USER_ID like 'PUBLIC%' and name = 'cdeCart'
-and last_write_date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 DAY);
