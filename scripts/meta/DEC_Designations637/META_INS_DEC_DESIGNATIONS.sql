6a231aedc07bda0f00feedf6bdbd7f8263804bd7
trushi2 <irina.trush@nih.gov>
2016-10-19 11:05:37 -0400

    Add files via upload

--- scripts/meta/DEC_Designations637/META_INS_DEC_DESIGNATIONS.sql
+++ scripts/meta/DEC_Designations637/META_INS_DEC_DESIGNATIONS.sql
@@ -1,40 +1,43 @@
-CREATE OR REPLACE procedure SBR.MDSR_INS_DEC_DESIGNATIONS
+set serveroutput on size 1000000
+SPOOL cadsrmeta-637p.log
+
+CCREATE OR REPLACE procedure SBR.META_INS_DEC_DESIGNATIONS
 as
-cursor c_desig is select * from temp_designations for update;
+cursor c_desig is select * from MDSR_DESIGNATIONS_UPLOAD for update;
 ac_id DESIGNATIONS.AC_IDSEQ%TYPE;
-con_id DESIGNATIONS.CONTE_IDSEQ%TYPE;
-cursor c_ins is select * from temp_designations where conte_idseq is not null and ac_idseq is not null;
+con_id DESIGNATIONS.CONTE_IDSEQ%TYPE;   
+cursor c_ins is select * from MDSR_DES_UPLOAD_VW where conte_idseq is not null and ac_idseq is not null;
 t_desig_id designations.desig_idseq%TYPE;
 errm varchar2(200);
 begin
 for i in c_desig loop
     begin
     select conte_idseq into con_id from contexts_view where name = i.context;
-    update temp_designations set conte_idseq = con_id where current of c_desig;
+    update MDSR_DESIGNATIONS_UPLOAD set conte_idseq = con_id where current of c_desig;
     exception
     when NO_DATA_FOUND then
-    insert into designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
+    insert into MDSR_designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
         DATECREATED, DATEMODIFIED, ID, LANGUAGENAME, MODIFIEDBY,
         NAME, TYPE2, DESIGNATIONCLASSSCHEMEITEMCOL, CONTEXT, AC_IDSEQ,
         CONTE_IDSEQ, COMMENTS, LOADDATE) values (i.PUBLICID, i.VERSION, i.LONGNAME, i.TYPE, i.CREATEDBY,
         i.DATECREATED, i.DATEMODIFIED, i.ID, i.LANGUAGENAME, i.MODIFIEDBY,
         i.NAME, i.TYPE2, i.DESIGNATIONCLASSSCHEMEITEMCOL, i.CONTEXT, i.AC_IDSEQ,
-        i.CONTE_IDSEQ, 'Context ID Not Found.SBR.META_INS_DESIGNATIONS', sysdate);
-    end;
+        i.CONTE_IDSEQ, 'Context ID Not Found.', sysdate);
+         end;
 
     Begin
-     select DEC_IDSEQ into ac_id from DATA_ELEMENT_CONCEPTS  where dec_id = i.publicid and version = i.version;
-    update temp_designations set ac_idseq = ac_id where current of c_desig;
-    EXCEPTION
+    select DEC_IDSEQ into ac_id from DATA_ELEMENT_CONCEPTS  where dec_id = i.publicid and version = i.version;
+    update MDSR_DESIGNATIONS_UPLOAD set ac_idseq = ac_id where current of c_desig;
+       EXCEPTION
     WHEN NO_DATA_FOUND then
-    insert into designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
+    insert into MDSR_designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
         DATECREATED, DATEMODIFIED, ID, LANGUAGENAME, MODIFIEDBY,
         NAME, TYPE2, DESIGNATIONCLASSSCHEMEITEMCOL, CONTEXT, AC_IDSEQ,
         CONTE_IDSEQ, COMMENTS, LOADDATE) values (i.PUBLICID, i.VERSION, i.LONGNAME, i.TYPE, i.CREATEDBY,
         i.DATECREATED, i.DATEMODIFIED, i.ID, i.LANGUAGENAME, i.MODIFIEDBY,
         i.NAME, i.TYPE2, i.DESIGNATIONCLASSSCHEMEITEMCOL, i.CONTEXT, i.AC_IDSEQ,
-        i.CONTE_IDSEQ, 'DEC ID Not Found.SBR.META_INS_DESIGNATIONS', sysdate);
-    End;
+        i.CONTE_IDSEQ, 'DEC ID Not Found.', sysdate);
+        End;
 end loop;
 commit;
 dbms_output.put_line ('Update Complete....');
@@ -43,17 +46,18 @@ for i in c_ins loop
    begin
    Insert into SBR.DESIGNATIONS
    (DESIG_IDSEQ, AC_IDSEQ, CONTE_IDSEQ, NAME, DETL_NAME, DATE_CREATED, CREATED_BY, LAE_NAME) VALUES (t_desig_id, i.ac_idseq, i.conte_idseq,i.name,i.type2  , sysdate, i.createdby, i.languagename);
-
+commit;
    EXCEPTION
    WHEN OTHERS then
-   errm := substr(SQLERRM,150);
-   insert into designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
+   errm := SQLERRM;
+   insert into MDSR_designations_load_err (PUBLICID, VERSION, LONGNAME, TYPE, CREATEDBY,
         DATECREATED, DATEMODIFIED, ID, LANGUAGENAME, MODIFIEDBY,
         NAME, TYPE2, DESIGNATIONCLASSSCHEMEITEMCOL, CONTEXT, AC_IDSEQ,
         CONTE_IDSEQ, COMMENTS, LOADDATE) values (i.PUBLICID, i.VERSION, i.LONGNAME, i.TYPE, i.CREATEDBY,
         i.DATECREATED, i.DATEMODIFIED, i.ID, i.LANGUAGENAME, i.MODIFIEDBY,
         i.NAME, i.TYPE2, i.DESIGNATIONCLASSSCHEMEITEMCOL, i.CONTEXT, i.AC_IDSEQ,
-        i.CONTE_IDSEQ,'SP META_INS_DESIGNATIONS..Error :'||errm, sysdate);
+        i.CONTE_IDSEQ,'Error During insert '|| errm, sysdate);
+        commit;
    end;
    end loop;
 commit;
@@ -61,3 +65,4 @@ commit;
 end;
 /
 exec SBR.MDSR_INS_DEC_DESIGNATIONS;
+SPOOL OFF
