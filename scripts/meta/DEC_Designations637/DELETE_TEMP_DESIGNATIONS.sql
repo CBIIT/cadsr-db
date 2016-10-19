ece8fc6e9e6e69dad89e3fd54c274035751a5af9
trushi2 <irina.trush@nih.gov>
2016-10-19 12:47:05 -0400

    Add files via upload

--- scripts/meta/DEC_Designations637/DELETE_TEMP_DESIGNATIONS.sql
+++ scripts/meta/DEC_Designations637/DELETE_TEMP_DESIGNATIONS.sql
@@ -2,10 +2,10 @@ set serveroutput on size 1000000
 SPOOL cadsrmeta-637t.log
 delete from temp_designations;
 delete from designations_load_err where loaddate<sysdate-40;
-rename  temp_designations to MDSR_designations_upload;
-rename MDSR_designations_loaderr to  MDSR_designations_load_err;
+rename temp_DESIGNATIONS to MDSR_DESIGNATIONS_UPLOAD;
+rename DESIGNATIONS_load_err to MDSR_designations_load_err;
 create view MDSR_DES_UPLOAD_VW as select distinct * from MDSR_designations_upload;
-SPOL OFF
+SPOOL OFF
 
 
 
