102ecfd6ff58419e3c19709c6362842e3e3ba159
trushi2 <irina.trush@nih.gov>
2016-10-21 10:41:13 -0400

    Add files via upload

---
+++ scripts/meta/UpdateEndDate638/UPDATE_PV_ENDDATE.sql
@@ -0,0 +1,22 @@
+update PERMISSIBLE_VALUES set end_date=to_date('10/06/2016','mm/dd/yyyy') 
+where PV_IDSEQ in(
+select p.PV_IDSEQ from VD_PVS v,
+value_domains d,
+PERMISSIBLE_VALUES p
+where d.VD_ID=2181490 
+and v.VD_IDSEQ = d.VD_IDSEQ
+ and v.conte_idseq=d.conte_idseq 
+ and p.PV_IDSEQ=v.PV_IDSEQ
+ and p.value in ('progression/relapse','Toxicities','Unknown')
+ and d.version=1); 
+update VD_PVS set end_date=to_date('10/06/2016','mm/dd/yyyy') 
+where VP_IDSEQ in(
+select v.VP_IDSEQ from VD_PVS v,
+value_domains d,
+PERMISSIBLE_VALUES p
+where d.VD_ID=2181490 
+and v.VD_IDSEQ = d.VD_IDSEQ
+ and v.conte_idseq=d.conte_idseq 
+ and p.PV_IDSEQ=v.PV_IDSEQ
+ and p.value in ('progression/relapse','Toxicities','Unknown')
+ and d.version=1);
