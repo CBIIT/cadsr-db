258c3f2f61110bca21822f8ba444189fe7ecb5cd
asafieva <natalia.asafieva@nih.gov>
2016-10-28 16:03:46 -0400

    CDEBROWSER-751 Update Object Cart URL to HTTPS script

---
+++ scripts/UpdateToolUrlsCDEBrowser.sql
@@ -0,0 +1,6 @@
+update tool_options_ext set value = 'https' || substr(value, INSTR(value,':',1,1)),
+DATE_MODIFIED = sysdate, MODIFIED_BY = user
+WHERE property = 'URL' and value like 'http:%' and TOOL_NAME='ObjectCartAPI'
+/
+commit
+/
