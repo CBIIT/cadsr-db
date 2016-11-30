28915253693f4400467fdd2073d38e6fc93a70c9
trushi2 <irina.trush@nih.gov>
2016-11-30 14:47:35 -0500

    Add files via upload

--- scripts/meta/loadEVSconcept580/UPLOAD_CONCEPTS.sql
+++ scripts/meta/loadEVSconcept580/UPLOAD_CONCEPTS.sql
@@ -1,21 +1,5 @@
-drop TABLE SBREXT.META_CONCEPTS_EXT_TEMP
-/
-drop TABLE SBREXT.META_CONCEPTS_EXT_ERROR_LOG
-/
-CREATE TABLE SBREXT.META_CONCEPTS_EXT_TEMP
-(    PREFERRED_NAME        VARCHAR2(300 BYTE)      ,
-  LONG_NAME             VARCHAR2(500 BYTE),
-  PREFERRED_DEFINITION  VARCHAR2(3000 BYTE)    
- )
-/
-CREATE TABLE SBREXT.META_CONCEPTS_EXT_ERROR_LOG
-( ERROR_MSG             VARCHAR2(3000 BYTE) ,
- UPLOAD_DATE            DATE,
-  PREFERRED_NAME        VARCHAR2(300 BYTE)      ,
-  LONG_NAME             VARCHAR2(500 BYTE),
-  PREFERRED_DEFINITION  VARCHAR2(3000 BYTE)    
-)
-/
+set serveroutput on size 1000000
+SPOOL cadsrmeta-580b.log
 
 CREATE OR REPLACE PROCEDURE SBREXT.UPLOAD_VALIDATE_CONCEPTS AS 
 
@@ -23,7 +7,7 @@ cursor c_load is
  select PREFERRED_NAME ,  LONG_NAME ,  PREFERRED_DEFINITION   
  from
  (select count(*) cnt,trim(PREFERRED_NAME) PREFERRED_NAME,  trim(LONG_NAME) LONG_NAME,
-  trim(PREFERRED_DEFINITION)PREFERRED_DEFINITION from SBREXT."META_CONCEPTS_EXT_TEMP"
+  trim(PREFERRED_DEFINITION)PREFERRED_DEFINITION from SBREXT."MDSR_CONCEPTS_EXT_TEMP"
   where PREFERRED_NAME is not null
    group BY PREFERRED_NAME ,  LONG_NAME ,  PREFERRED_DEFINITION)
 --   where PREFERRED_NAME='C126939'
@@ -43,7 +27,7 @@ for i in c_load loop
 
 begin     
 
-errmsg:='A';    
+   
 select CONTE_IDSEQ into v_CONTE_IDSEQ from contexts
               where trim(name)='NCIP' and version=1; 
 SELECT count(*) into v_cnt
@@ -80,7 +64,7 @@ SELECT count(*) into v_cnt
             END IF;
             IF length(trim(errmsg))>1 then
              dbms_output.put_line('errmsg2 - '||errmsg);
-            insert into META_CONCEPTS_EXT_ERROR_LOG VALUES (errmsg,sysdate,i.PREFERRED_NAME,i.LONG_NAME ,i.PREFERRED_DEFINITION);
+            insert into MDSR_CONCEPTS_EXT_ERROR_LOG VALUES (errmsg,sysdate,i.PREFERRED_NAME,i.LONG_NAME ,i.PREFERRED_DEFINITION);
               
            ELSE
            
@@ -115,21 +99,21 @@ SELECT count(*) into v_cnt
                    v_CONTE_IDSEQ,                  --CONTE_IDSEQ           NOT NULL,
                    1,                              --  VERSION             NOT NULL,
                    'RELEASED',                     -- ASL_NAME             NOT NULL,
-                   'YES',                          --LATEST_VERSION_IND 
+                   'Yes',                          --LATEST_VERSION_IND 
                    'NCI',                          ---DEFINITION_SOURCE
                     sysdate,                      --DATE_CREATED
                    'NCI Thesaurus',               --ORIGIN   
                    'SBREXT',                      -- CREATED_BY
                     v_CON_ID,                     --CON_ID NOT NULL,
                    'EVS_CONCEPT_CODE' );          --EVS_SOURCE
-                    
+    commit;                
             END IF;
   
   EXCEPTION
     WHEN OTHERS THEN
         errmsg := SQLERRM;
         dbms_output.put_line('errmsg3 - '||errmsg);
-                  insert into META_CONCEPTS_EXT_ERROR_LOG VALUES (errmsg,sysdate,i.PREFERRED_NAME,i.LONG_NAME ,i.PREFERRED_DEFINITION);
+                  insert into MDSR_CONCEPTS_EXT_ERROR_LOG VALUES (errmsg,sysdate,i.PREFERRED_NAME,i.LONG_NAME ,i.PREFERRED_DEFINITION);
  end;   
   end loop;
 
@@ -137,4 +121,5 @@ commit;
 
 END ;
 /
-
+exec SBREXT.UPLOAD_VALIDATE_CONCEPTS;
+SPOOL OFF
