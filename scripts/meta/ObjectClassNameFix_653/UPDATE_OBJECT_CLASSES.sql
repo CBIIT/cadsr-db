34ce22035d5666a7a9338672f12a81f5458d7d0d
trushi2 <irina.trush@nih.gov>
2016-12-29 11:57:36 -0500

    Add files via upload

---
+++ scripts/meta/ObjectClassNameFix_653/UPDATE_OBJECT_CLASSES.sql
@@ -0,0 +1,10 @@
+set serveroutput on size 1000000
+SPOOL cadsrmeta-653.log
+
+  update SBREXT.OBJECT_CLASSES_EXT   
+  set ='MD Anderson Symptom Inventory - Head and Neck'
+  where PREFERRED_NAME='C105551'
+  and OC_ID=3698757
+  and version=1;
+  commit;
+  SPOOL OFF
