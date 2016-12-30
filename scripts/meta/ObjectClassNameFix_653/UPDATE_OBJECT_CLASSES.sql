5d0ba1006de19ccdb64bd26a9e88c10aa3f35ada
trushi2 <irina.trush@nih.gov>
2016-12-30 10:23:13 -0500

    Add files via upload

--- scripts/meta/ObjectClassNameFix_653/UPDATE_OBJECT_CLASSES.sql
+++ scripts/meta/ObjectClassNameFix_653/UPDATE_OBJECT_CLASSES.sql
@@ -2,7 +2,7 @@ set serveroutput on size 1000000
 SPOOL cadsrmeta-653.log
 
   update SBREXT.OBJECT_CLASSES_EXT   
-  set ='MD Anderson Symptom Inventory - Head and Neck'
+  set LONG_NAME='MD Anderson Symptom Inventory - Head and Neck'
   where PREFERRED_NAME='C105551'
   and OC_ID=3698757
   and version=1;
