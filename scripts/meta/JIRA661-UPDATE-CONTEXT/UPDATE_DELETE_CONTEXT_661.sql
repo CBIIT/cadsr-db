efbb2f6c6e85e74ba1bf954ef3446ce7499de257
trushi2 <irina.trush@nih.gov>
2017-02-23 11:32:53 -0500

    Delete UPDATE_DELETE_CONTEXT_661.sql

--- scripts/meta/JIRA661-UPDATE-CONTEXT/UPDATE_DELETE_CONTEXT_661.sql
+++
@@ -1,7 +0,0 @@
-set serveroutput on size 1000000
-SPOOL cadsrmeta-661b.log
-
-exec SBR.MDSR_UPD_CONTE_IDSEQ ('CDISC' ,'TEST','DWARZEL');
-exec SBR.MDSR_DELETE_CONTEXT('CDISC' ,'DWARZEL');;
-
-SPOOL OFF
