de86b25e274a5a3e25db17ba38d21590ca98d44c
trushi2 <irina.trush@nih.gov>
2018-03-11 14:27:51 -0400

    Add files via upload

---
+++ scripts/meta/JIRA_711_MDSR_FB_XML/FB_XML_OBJECTS.sql
@@ -0,0 +1,191 @@
+
+
+CREATE OR REPLACE TYPE          "MDSR_FB_DESIGNATIONS_XML_T"          as object(
+"createdBy"                                      VARCHAR2(100)
+,"dateCreated"                         VARCHAR2(30)
+,"languageName"                                           VARCHAR2(30) 
+,"name"                                               VARCHAR2(2000)
+,"type"                                      VARCHAR2(20)--"DETL_NAME"
+,"context"                                     VARCHAR2(30) 
+   );
+/
+CREATE OR REPLACE TYPE MDSR_FB_DESIGN_XML_LIST_T AS TABLE OF  MDSR_FB_DESIGNATIONS_XML_T;
+
+
+CREATE OR REPLACE TYPE          "MDSR_FB_VM_XML_T4"          as object(
+"publicID"          number,
+"version"             number (4,2),
+"longname"        varchar2 (300) ,
+"designation"    MDSR_FB_DESIGN_XML_LIST_T,
+"preferredDefinition"  VARCHAR2(2000));
+/
+CREATE OR REPLACE TYPE  "MDSR_FB_VM_XML_LIST_T4"  AS TABLE OF  MDSR_FB_VM_XML_T4;
+/
+CREATE OR REPLACE TYPE          "MDSR_FB_PV_XML_T4"          as object(
+ 
+ "value"       varchar2 (300) ,
+"valueMeaning"    MDSR_FB_VM_XML_LIST_T4);
+/
+CREATE OR REPLACE TYPE  "MDSR_FB_PV_XML_LIST_T4"  AS TABLE OF  MDSR_FB_PV_XML_T4;
+/
+CREATE OR REPLACE TYPE          "MDSR_FB_VD_XML_T4"                                          AS OBJECT
+(   
+  "longName"      varchar2(255),
+  "publicId"         number,
+  "version"                number (4,2),
+  "type"        varchar2 (50),
+  "workflowStatus"         varchar2 (20), 
+  "Datatype"               varchar2 (20),   
+  "MaximumLength"          number (8),
+  "MinimumLength"          number (8),
+  "PermissibleValue"    MDSR_FB_PV_XML_LIST_T4
+);
+/
+CREATE OR REPLACE TYPE  "MDSR_FB_VD_XML_LIST_T4"  AS TABLE OF  MDSR_FB_VD_XML_T4;
+/
+/
+
+CREATE OR REPLACE TYPE          "MDSR_FB_DATA_EL_XML_T4"                                          AS OBJECT
+(   
+  "longName"      varchar2(255),
+  "shortName"   varchar2(60),
+  "publicId"         number,
+  "version"                number (4,2),
+  "context"    VARCHAR2(40),
+  "workflowStatus"         varchar2 (20), 
+  "preferredDefinition" VARCHAR2(2000),
+  "valueDomain"    MDSR_FB_VD_XML_LIST_T4
+);
+
+CREATE OR REPLACE TYPE  "MDSR_FB_DATA_EL_XML_LIST_T4"  AS TABLE OF  MDSR_FB_DATA_EL_XML_T4;
+CREATE OR REPLACE TYPE               MDSR_FB_PROTOCOL_XML_T as object(
+ "leadOrganization" VARCHAR2(50),
+ "phase" VARCHAR2(5),
+"type" VARCHAR2(250),
+"protocolID" VARCHAR2(50),
+"longName"  VARCHAR2(255),
+"context"   VARCHAR2(10),
+"shortName"  VARCHAR2(50),
+"preferredDefinition"  VARCHAR2(2000))
+/
+CREATE OR REPLACE TYPE  "MDSR_FB_PROTOCOL_XML_LIST_T"  AS TABLE OF  MDSR_FB_PROTOCOL_XML_T
+/
+CREATE OR REPLACE TYPE                MDSR_FB_MODULE_XML_T    as object(
+"displayOrder"                                     NUMBER
+,"maximumModuleRepeat"                              NUMBER
+,"createdBy"   VARCHAR2(40) 
+,"publicid"  NUMBER
+,"version"  NUMBER
+
+, "preferredDefinition" VARCHAR2(2000)
+
+)
+/
+CREATE OR REPLACE TYPE  "MDSR_FB_MODULE_XML_LIST_T"  AS TABLE OF  MDSR_FB_MODULE_XML_T
+/
+CREATE OR REPLACE TYPE             SBREXT.MDSR_FB_FORM_XML_T    as object(    
+"context"                                    VARCHAR2(40)
+,"createdBy"                                      VARCHAR2(100)
+,"dateCreated"                         VARCHAR2(30)
+,"dateModified"    VARCHAR2(30)
+,"modifiedBy"   VARCHAR2(100)
+,"longName"  VARCHAR2(255)   
+,"changeNote"                              VARCHAR2(2000)
+,"preferredDefinition"                      VARCHAR2(2000)
+,"cadsrRAI"             VARCHAR2(40)
+,"publicid"  NUMBER
+,"version"  NUMBER
+,"workflowStatusName" VARCHAR2(40)
+,"categoryName" VARCHAR2(255) 
+,"type"              VARCHAR2(5) 
+--,"headerInstruction"     SBREXT.REDCAP_HINSTRUCTIONS_T                       
+--,"footerInstruction"  VARCHAR2(100)
+,"module" SBREXT.MDSR_FB_MODULE_XML_T
+,"protocol" SBREXT.MDSR_FB_PROTOCOL_XML_T)
+/
+
+      CREATE OR REPLACE FORCE VIEW SBREXT.MDSR_XML_FB_FORMS_VIEW
+(
+   QC_IDSEQ,
+   VERSION,
+   TYPE,
+   CONTE_IDSEQ,
+   CATEGORY_NAME,
+   WORKFLOW,
+   PREFERRED_NAME,
+   DEFINITION,
+   LONG_NAME,  
+   CONTEXT_NAME,
+   CREATED_BY,
+   DATE_CREATED,
+   DATE_MODIFIED,
+   PUBLIC_ID,
+   CHANGE_NOTE,
+   LATEST_VERSION_IND
+)
+AS
+     SELECT crf.QC_IDSEQ,
+            crf.VERSION,
+            crf.QTL_NAME,
+            crf.CONTE_IDSEQ,
+            crf.QCDL_NAME,
+            crf.ASL_NAME,
+            crf.PREFERRED_NAME,
+            crf.PREFERRED_DEFINITION,
+            crf.LONG_NAME,      
+            cnt.NAME,
+            crf.CREATED_BY,
+            crf.DATE_CREATED,
+            crf.DATE_MODIFIED,            
+            crf.QC_ID,
+            crf.change_note,
+            crf.LATEST_VERSION_IND
+       FROM QUEST_CONTENTS_EXT crf,
+            CONTEXTS cnt
+          
+      WHERE  (crf.QTL_NAME = 'CRF' OR crf.QTL_NAME = 'TEMPLATE')
+            AND crf.ASL_NAME='RELEASED'
+            AND crf.CONTE_IDSEQ = cnt.CONTE_IDSEQ
+            AND instr(cnt.NAME,'TEST')=0
+            AND instr(NAME,'Training')=0
+   ORDER BY crf.LONG_NAME;
+CREATE OR REPLACE FORCE VIEW SBREXT.MDSR_XML_FB_MODULES_VIEW
+(
+   MOD_IDSEQ,
+   CRF_IDSEQ,
+   MOD_ID,
+   VERSION,
+   CONTE_IDSEQ,
+   WORKFLOW,
+   PREFERRED_NAME,
+   DEFINITION,
+   LONG_NAME,
+   LATEST_VERSION_IND,
+   QR_IDSEQ,
+   P_QC_IDSEQ,
+   C_QC_IDSEQ,
+   DISPLAY_ORDER,
+   REPEAT_NO
+)
+AS
+     SELECT MOD.QC_IDSEQ,
+            MOD.DN_CRF_IDSEQ,
+            MOD.QC_ID,
+            MOD.VERSION,
+            MOD.CONTE_IDSEQ,
+            MOD.ASL_NAME,
+            MOD.PREFERRED_NAME,
+            MOD.PREFERRED_DEFINITION,
+            MOD.LONG_NAME,
+            MOD.LATEST_VERSION_IND,
+            qr.QR_IDSEQ,
+            qr.P_QC_IDSEQ,
+            qr.C_QC_IDSEQ,
+            qr.DISPLAY_ORDER,
+            MOD.repeat_no
+       FROM SBREXT.QUEST_CONTENTS_EXT MOD, QC_RECS_EXT qr
+      WHERE     MOD.QC_IDSEQ = qr.C_QC_IDSEQ
+            AND MOD.qtl_name = 'MODULE'
+            AND MOD.DELETED_IND = 'No'
+            AND qr.RL_NAME = 'FORM_MODULE'
+   ORDER BY MOD.DN_CRF_IDSEQ, qr.DISPLAY_ORDER;
