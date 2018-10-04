a537f7c02b062844ed7494e5c1563c9d1470103e
trushi2 <irina.trush@nih.gov>
2018-10-04 11:33:46 -0400

    Add files via upload

--- scripts/meta/JIRA-160-CADSRPASSW/CADSRPASSW-160.sql
+++ scripts/meta/JIRA-160-CADSRPASSW/CADSRPASSW-160.sql
@@ -1,11 +1,12 @@
  set serveroutput on size 1000000
 SPOOL CADSRPASSW-160.log
-UPDATE SBREXT.TOOL_OPTIONS_EXT 
- set value ='Your password for the caDSR account ${userid} is about to expire on ${expiryDate}. To change your password, you can either login to the Password 
- Change Station by visiting https://${webHost} or contact the NCI Helpdesk at NCIAppSupport@nih.gov.'
+UPDATE SBREXT.TOOL_OPTIONS_EXT  
+ set value ='Your password for the caDSR account ${userid} is about to expire on ${expiryDate}. To change your password, you can either login to the Password Change Station by visiting https://${webHost} or contact the NCI Helpdesk at NCIAppSupport@nih.gov.'
  where 
- TOOL_NAME='PasswordChangeStation' and PROPERTY = 'EMAIL.INTRO';
- commit;
+ TOOL_NAME='PasswordChangeStation' and PROPERTY = 'EMAIL.INTRO'
+ /
+ commit
+ /
  SPOOL  OFF
  
 
