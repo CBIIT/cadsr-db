c859f5fd569b6ac9035b21557bebf6f311b19396
asafieva <natalia.asafieva@nih.gov>
2018-11-13 11:13:40 -0500

    CADSRPASSW-163 SQL script

---
+++ scripts/meta/JIRA-163-PWCS-LOGO-URL/update-pwcs-logo-url-163.sql
@@ -0,0 +1,10 @@
+set serveroutput on size 1000000
+SPOOL cadsrpassw-163.log
+set define off
+update SBREXT.TOOL_OPTIONS_EXT set 
+value='https://cbiit.nci.nih.gov/ncip/biomedical-informatics-resources/interoperability-and-semantics/metadata-and-models'
+where TOOL_NAME like 'PasswordChangeStation' and property='LOGO.ROOT'
+/
+commit
+/
+SPOOL OFF
