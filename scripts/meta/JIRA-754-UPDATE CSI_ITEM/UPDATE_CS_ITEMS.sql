c8166a851d29bfb75d57a37e1fcf87afa813d503
trushi2 <irina.trush@nih.gov>
2019-06-24 16:10:43 -0400

    Update UPDATE_CS_ITEMS.sql

--- scripts/meta/JIRA-754-UPDATE CSI_ITEM/UPDATE_CS_ITEMS.sql
+++ scripts/meta/JIRA-754-UPDATE CSI_ITEM/UPDATE_CS_ITEMS.sql
@@ -3,7 +3,7 @@ SPOOL cadsrmeta-754.log
 
 update sbrext.CS_ITEMS set long_name='QIN Breast Study',
 modified_by='SBREXT',change_note='User Request',
-date_modified=SYSDATE  where CSI_ID=6788757;
+date_modified=SYSDATE  where CSI_IDSEQ='8BDBB6CA-0218-3356-E053-F662850AE8E3';
 commit;
 
 SPOOL OFF
