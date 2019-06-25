c9a866e3850b7b3e9a22f289ff3a72ff1f606e86
trushi2 <irina.trush@nih.gov>
2019-06-25 14:37:28 -0400

    Add files via upload

--- scripts/meta/JIRA-754-UPDATE CSI_ITEM/UPDATE_CS_ITEMS.sql
+++ scripts/meta/JIRA-754-UPDATE CSI_ITEM/UPDATE_CS_ITEMS.sql
@@ -1,9 +1,14 @@
 set serveroutput on size 1000000
-SPOOL cadsrmeta-754.log  
+SPOOL cadsrmeta-749.log  
 
 update sbrext.CS_ITEMS set long_name='QIN Breast Study',
-modified_by='SBREXT',change_note='User Request',
-date_modified=SYSDATE  where CSI_IDSEQ='8BDBB6CA-0218-3356-E053-F662850AE8E3';
+modified_by='SBREXT',change_note='Long_name modified by User Request',
+date_modified=SYSDATE  
+where CSI_NAME='QIN' 
+and preferred_name ='6788757' 
+and version=1 
+and CSITL_NAME='GROUP_TYPE'
+and conte_idseq=(select conte_idseq from CONTEXTS where name ='NCIP' and version=1);
 commit;
 
 SPOOL OFF
