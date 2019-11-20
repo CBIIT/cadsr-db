12bdd90ac663525b2c7fb8a21a89945e1645da35
trushi2 <irina.trush@nih.gov>
2019-11-20 11:48:38 -0500

    Update CREATE_MDSR759_OBJECTS_GIT.sql

--- scripts/meta/JIRA-759_CS_CSI_XML/CREATE_MDSR759_OBJECTS_GIT.sql
+++ scripts/meta/JIRA-759_CS_CSI_XML/CREATE_MDSR759_OBJECTS_GIT.sql
@@ -533,7 +533,7 @@ DBMS_SCHEDULER.CREATE_JOB (
    job_name           =>  'MSDR_GEN_XML_CLCSI_JOB',
    job_type           =>  'STORED_PROCEDURE',
    job_action         =>  'SBREXT.MSDR_GEN_XML_CLCSI',
-   start_date         =>  '13-NOV-19 10.00.00 PM',
+   start_date         =>  '13-NOV-19 11.00.00 PM',
    repeat_interval    =>  'FREQ=DAILY',
    enabled            =>  TRUE);
 
