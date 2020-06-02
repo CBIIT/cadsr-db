29152561685b073d5912650e24476159386a8d1f
trushi2 <irina.trush@nih.gov>
2020-06-02 15:55:51 -0400

    Add files via upload

---
+++ scripts/meta/JIRA-806-CADSRMETA-upload-designations/DELETE_TEMP_DESIGNATIONS.sql
@@ -0,0 +1,9 @@
+set serveroutput on size 1000000
+SPOOL cadsrmeta-637d.log
+
+delete from SBREXT.MDSR_DESIGNATIONS_UPLOAD;
+commit;
+delete from SBREXT.MDSR_designations_load_err;
+commit;
+
+SPOOL OFF
